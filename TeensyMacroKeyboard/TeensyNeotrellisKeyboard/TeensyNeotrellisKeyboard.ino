71e8b5f94d7f8a47f7d4717a6c133b164cd1bbb8
Ben Von Handorf <ben@skyironstudio.com>
2020-12-30 21:48:52 -0500

    Macro keyboard WIP

--- TeensyMacroKeyboard/TeensyNeotrellisKeyboard/TeensyNeotrellisKeyboard.ino
+++ TeensyMacroKeyboard/TeensyNeotrellisKeyboard/TeensyNeotrellisKeyboard.ino
@@ -13,27 +13,128 @@ struct KeyData KEYS[16] = {
   {
     .noteId = 60,
     .color = {255, 0, 0, 0}
-  }
+  },
+  {
+    .noteId = 61,
+    .color = {255, 0, 0, 0}
+  },
+  {
+    .noteId = 62,
+    .color = {255, 0, 0, 0}
+  },
+  {
+    .noteId = 63,
+    .color = {255, 0, 0, 0}
+  },
+  {
+    .noteId = 64,
+    .color = {0, 255, 0, 0}
+  },
+  {
+    .noteId = 65,
+    .color = {0, 255, 0, 0}
+  },
+  {
+    .noteId = 66,
+    .color = {0, 255, 0, 0}
+  },
+  {
+    .noteId = 67,
+    .color = {0, 255, 0, 0}
+  },
+  {
+    .noteId = 68,
+    .color = {128, 128, 0, 0}
+  },
+  {
+    .noteId = 69,
+    .color = {128, 128, 0, 0}
+  },
+  {
+    .noteId = 70,
+    .color = {128, 128, 0, 0}
+  },
+  {
+    .noteId = 71,
+    .color = {128, 128, 0, 0}
+  },
+  {
+    .noteId = 72,
+    .color = {128, 0, 128, 0}
+  },
+  {
+    .noteId = 73,
+    .color = {128, 0, 128, 0}
+  },
+  {
+    .noteId = 74,
+    .color = {128, 0, 128, 0}
+  },
+  {
+    .noteId = 75,
+    .color = {128, 0, 128, 0}
+  },
 };
 
-TrellisCallback blink(keyEvent evt){
+Encoder myEnc(5, 6);
+
+const int CHANNEL = 1;
+
+TrellisCallback event(keyEvent event) {
+  struct KeyData *key = &KEYS[event.bit.NUM];
   
-  if(evt.bit.EDGE == SEESAW_KEYPAD_EDGE_RISING)
-    trellis.pixels.setPixelColor(evt.bit.NUM, Wheel(map(evt.bit.NUM, 0, trellis.pixels.numPixels(), 0, 255))); //on rising
-  else if(evt.bit.EDGE == SEESAW_KEYPAD_EDGE_FALLING)
-    trellis.pixels.setPixelColor(evt.bit.NUM, 0); //off falling
+  if(event.bit.EDGE == SEESAW_KEYPAD_EDGE_RISING) {
+    usbMIDI.sendNoteOn(key->noteId, 99, CHANNEL);
+    trellis.pixels.setPixelColor(event.bit.NUM, trellis.pixels.Color(key->color[0], key->color[1], key->color[2]));
+  } else if(event.bit.EDGE == SEESAW_KEYPAD_EDGE_FALLING) {
+    usbMIDI.sendNoteOff(key->noteId, 99, CHANNEL);
+    trellis.pixels.setPixelColor(event.bit.NUM, 0); //off falling
+  }
     
   trellis.pixels.show();
+  
   return 0;
 }
 
 
 void setup() {
-  // put your setup code here, to run once:
+Serial.begin(115200);
+  //while(!Serial);
+
+  pinMode(INT_PIN, INPUT);
+  
+  if(!trellis.begin()){
+    Serial.println("could not start trellis");
+    while(1);
+  }
+  else{
+    Serial.println("trellis started");
+  }
+
+  //activate all keys and set callbacks
+  for(int i=0; i<NEO_TRELLIS_NUM_KEYS; i++){
+    trellis.activateKey(i, SEESAW_KEYPAD_EDGE_RISING);
+    trellis.activateKey(i, SEESAW_KEYPAD_EDGE_FALLING);
+    trellis.registerCallback(i, event);
+  }
+
+  //do a little animation to show we're on
+  for(uint16_t i=0; i<trellis.pixels.numPixels(); i++) {
+    struct KeyData *key = &KEYS[i];
+    trellis.pixels.setPixelColor(i, trellis.pixels.Color(key->color[0], key->color[1], key->color[2]));
+    trellis.pixels.show();
+    delay(50);
+  }
+  for(uint16_t i=0; i<trellis.pixels.numPixels(); i++) {
+    trellis.pixels.setPixelColor(i, 0x000000);
+    trellis.pixels.show();
+    delay(50);
+  }
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(!digitalRead(INT_PIN)){
+    trellis.read(false);
+  }
 }
---
+++ eagle/APA_Stick/4-APA102-No_output.pro
@@ -0,0 +1,25 @@
+EAGLE AutoRouter Statistics:
+
+Job           : /Users/benvh/Documents/eagle/APA_Stick/4-APA102-No_output.brd
+
+Start at      : 09:25:04 (10/24/15)
+End at        : 09:25:06 (10/24/15)
+Elapsed time  : 00:00:00
+
+Signals       :     8   RoutingGrid: 9 mil  Layers: 2
+Connections   :    12   predefined:  0 ( 0 Vias )
+
+Router memory :   38656
+
+Passname          : TopRouter     Route Optimize1 Optimize2 Optimize3 Optimize4
+
+Time per pass     :  00:00:00  00:00:00  00:00:00  00:00:00  00:00:00  00:00:00
+Number of Ripups  :         0         0         0         0         0         0
+max. Level        :         0         0         0         0         0         0
+max. Total        :         0         0         0         0         0         0
+
+Routed            :        12        12        12        12        12        12
+Vias              :         0         0         0         0         0         0
+Resolution        :   100.0 %   100.0 %   100.0 %   100.0 %   100.0 %   100.0 %
+
+Final             : 100.0% finished
